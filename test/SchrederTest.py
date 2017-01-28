ff0f083d8b3f863c22f4a0d9edd900251f5c9d3c
Paul Water <p.r.water@setbased.nl>
2017-01-28 16:49:48 +0100

    Unit test voor shredder.

---
+++ test/SchrederTest.py
@@ -0,0 +1,69 @@
+from unittest import TestCase
+
+from cleo import Application
+from cleo import CommandTester
+
+from kerapu.command.ShredderCommand import ShredderCommand
+
+
+class ShredderTest(TestCase):
+    """
+    Test cases voor XML shredder.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_referentie(self):
+        """
+        Test extractie referentietabellen.
+        """
+        application = Application()
+        application.add(ShredderCommand())
+
+        command = application.find('kerapu:shredder')
+        command_tester = CommandTester(command)
+        status = command_tester.execute([('command', command.get_name()),
+                                         ('XML-bestand', 'test/var/lib/20170101 Referenties v20161117.xml'),
+                                         ('folder', 'test/var/lib')])
+
+        output = command_tester.get_display().rstrip()
+
+        self.assertEqual(0, status)
+
+        for tabel in ['AfsluitRedenen',
+                      'BehandelKlassen',
+                      'Diagnosen',
+                      'Geslachten',
+                      'LimitatieMachtigingen',
+                      'Specialismen',
+                      'VertaalZorgActiviteiten',
+                      'ZorgProductGroepen',
+                      'ZorgTypen',
+                      'ZorgVragen']:
+            self.assertRegex(output, 'Wrote \s*\d+ rows to {}.csv'.format(tabel), tabel)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_boom(self):
+        """
+        Test extractie boombestanden.
+        """
+        application = Application()
+        application.add(ShredderCommand())
+
+        command = application.find('kerapu:shredder')
+        command_tester = CommandTester(command)
+        status = command_tester.execute([('command', command.get_name()),
+                                         ('XML-bestand', 'test/var/lib/20170101 BoomBestanden v20161117.xml'),
+                                         ('folder', 'test/var/lib')])
+
+        output = command_tester.get_display().rstrip()
+
+        self.assertEqual(0, status)
+
+        for tabel in ['BeslisRegels',
+                      'AttribuutGroepen',
+                      'AttribuutGroepKoppelingen',
+                      'Attributen',
+                      'BoomParameters']:
+            self.assertRegex(output, 'Wrote \s*\d+ rows to {}.csv'.format(tabel), tabel)
+
+# ------------------------------------------------------------------------------------------------------------------
