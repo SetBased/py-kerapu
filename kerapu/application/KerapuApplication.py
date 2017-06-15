0e7f08f1ee37bd1509f0cac8ebbb24c159a4e10c
Paul Water <p.r.water@setbased.nl>
2017-06-15 12:10:28 +0200

    Release: 1.0.1

--- kerapu/application/KerapuApplication.py
+++ kerapu/application/KerapuApplication.py
@@ -16,7 +16,7 @@ class KerapuApplication(Application):
         """
         Object constructor.
         """
-        Application.__init__(self, 'kerapu', '1.0.0')
+        Application.__init__(self, 'kerapu', '1.0.1')
 
     # ------------------------------------------------------------------------------------------------------------------
     def get_default_commands(self):
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as handle:
 setup(
     name='Kerapu',
 
-    version='1.0.0',
+    version='1.0.1',
 
     description='Een implementatie van de grouper',
     long_description=long_description,
